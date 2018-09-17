59d118d9ae10849d47d388e428563eba14a8a320
Derek Pham <anhdpham97@gmail.com>
2018-09-16 20:25:33 -0400

    some working javascript

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # job-site-extract
+This is a firefox extension for the NuCareers website (Northeastern's).
+It's intended to extract the job information and send it to my local database.
+It should also stop NuCareers from switching to a new tab when opening a job (a very annoying feature).
+
+Hopefully, there will be a simple website soon to display all the jobs info extracted.
--- extract.js
+++ extract.js
@@ -1,1 +1,31 @@
-console.log("Hello, world");
+let allRows = document.getElementsByTagName("tr");
+for (var i = 0; i < allRows.length; i++) {
+    var row = allRows[i];
+    if (row.id.includes('posting')) {
+
+    }
+}
+
+for (var i = 0; i < allLinks.length; i++) {
+    let linkNode = allLinks[i];
+    let onclickContent = linkNode.getAttribute('onclick');
+    if (onclickContent != null && onclickContent != undefined) {
+        //linkNode.setAttribute('href', onclickContent);
+    }
+}
+
+console.log(tabs);
+
+function checkIsJobListingPage(document) {
+    let postingsTable = document.getElementById("postingsTable");
+    return postingsTable != null && postingsTable != undefined;
+}
+
+function checkIsSingleJobInfoPage(document) {
+    let postingDiv = document.getElementById("postingDiv");
+    return postingDiv != null && postingDiv != undefined;
+}
+
+function injectFocus(onClickStr) {
+    return onClickStr += "\nwindow.focus();";
+}
